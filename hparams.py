84e51efd1539c6267e4755b481004eceeb23ce03
yfes <42191873+yfes@users.noreply.github.com>
2022-01-23 22:04:05 +0100

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -27,7 +27,7 @@ def create_hparams(hparams_string=None, verbose=False):
         load_mel_from_disk=False,
         training_files='filelists/ljs_audio_text_train_filelist.txt',
         validation_files='filelists/ljs_audio_text_val_filelist.txt',
-        text_cleaners=['english_cleaners'],
+        text_cleaners=['basic_cleaners'],
 
         ################################
         # Audio Parameters             #
